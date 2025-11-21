8e83473321404be91e8d88bc4e6e28b82181e9d6
oliverpollo <olp003@edu.zealand.com>
2025-11-21 13:50:11 +0100

    Membership Class 1.2

--- HundeKlub/Membership.cs
+++ HundeKlub/Membership.cs
@@ -10,21 +10,40 @@ namespace HundeKlub
     {
         private int _membershipNumber;
         private double _rentingFee = 100;
-        private double _membershipFee = 1000;
+        private double _baseFee = 1000;
+        private int _numberOfDogs;
+        private static Membership _instance = null;
+        private static int _instanceCounter = 0;
+        private int _instanceNumber;
+
+
         public int MembershipNumber { get { return _membershipNumber; } set { _membershipNumber = value; } }
         public double RentingFee { get { return _rentingFee; } set { _rentingFee = value; } }
-        public double MembershipFee { get { return _membershipFee; } set { _membershipFee = value; } }
+        public double MembershipFee { get { return _baseFee; } set { _baseFee = value; } }
+        public int Age { get; set; }
         public Membership(int membershipNumber)
         {
             _membershipNumber = membershipNumber;
         }
+        private Membership()
+        {
+            _instanceCounter++;
+            _instanceNumber = _instanceCounter;
+        }
         public override string ToString()
         {
-            return $"Membership number: {_membershipNumber}\nRenting Fee: {_rentingFee}\nMembership Fee: {_membershipFee}";
+            return $"Membership number: {_membershipNumber}\nRenting Fee: {_rentingFee}\nMembership Fee: {_baseFee}";
         }
         public double CalculateTotalFee(int numberOfRentings)
         {
-            return _membershipFee + (_rentingFee * numberOfRentings);
+            return _baseFee + (_rentingFee * numberOfRentings);
+        }
+        public double MemberFee(double baseFee) 
+        { if (Age > 65)
+            {
+                baseFee = baseFee * 1.5;
+            }
+            return baseFee;
         }
         public double CalculateWeekendFee(int weekendRenting)
         {
@@ -33,10 +52,14 @@ namespace HundeKlub
         }
 
         public double CalculateMembershipFee() 
+        {
+            if (_instanceCounter > 1)
+                return _baseFee + 500; 
         { 
             if (hej)
         return _membershipFee + 500;
         }
+
 
 
     }
