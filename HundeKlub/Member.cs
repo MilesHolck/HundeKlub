a158b222e225b9013ac7e45529510ea07228463f
Ida Emilie Balch Hansen <idh004@edu.zealand.dk>
2025-11-21 12:41:33 +0100

    CRUD implementeret og testet.

--- HundeKlub/Member.cs
+++ HundeKlub/Member.cs
@@ -11,7 +11,7 @@ namespace HundeKlub
         private int _id;
         private string _name;
         private string _address;
-        private DateTime _birthdate;
+        private DateTime _birthDate;
         private string _phone;
         private string _email; 
 
@@ -19,23 +19,42 @@ namespace HundeKlub
         public int Id { get { return _id; } set { _id = value; } }
         public string Name { get { return _name; } set { _name = value; } } 
         public string Address { get { return _address; } set { _address = value; } }
-        public DateTime Birthdate { get { return _birthdate; } set { _birthdate = value; } } 
+        public DateTime BirthDate { get { return _birthDate; } set { _birthDate = value; } } 
         public string Phone { get { return _phone; } set { _phone = value; } } 
         public string Email { get { return _email; } set { _email = value; } }
 
         public List<Dog> DogList { get; set; } 
 
 
-        public Member(string name, List <Dog> dogList)
+        public Member(string name, DateTime birthdate, List <Dog> dogList)
         {
             Name = name;
-            DogList = dogList; 
+            DogList = dogList;
+            BirthDate = birthdate; 
         }
 
         public override string ToString()
         {
             return $"Name: {Name} \nDog: Dogs go here";
 
+        }
+
+        public void RegisterDog (Dog dog)
+        {
+            DogList.Add(dog); 
+        }
+
+        public void PrintDogs()
+        {
+            foreach (Dog dogs in DogList)
+            {
+                Console.WriteLine(dogs); 
+            }
+        }
+
+        public void RemoveDog (Dog dog)
+        {
+            DogList.Remove(dog); 
         }
 
     }
