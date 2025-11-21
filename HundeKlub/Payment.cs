de7c88528738c45a65eedf9e11ff02b6e312a274
camil <camilla-holck@hotmail.com>
2025-11-21 13:14:09 +0100

    Payment stuff

---
+++ HundeKlub/Payment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HundeKlub
+{
+    public class Payment
+    {
+        private int _id;
+        
+
+        public int Id { get { return _id; } set { _id = value; } }
+
+
+        public Payment(int id)
+        {
+            _id = id;
+        }
+
+        public void Recipt()
+        {
+            Console.WriteLine("Receipt printed");
+        }
+
+        public enum PaymentMethod
+        {
+            card,
+            cash,
+            mobilePay,
+            autoBankTransfer
+
+        }
+        public enum PaymentType
+        {
+            monthlyPayment,
+            payPerEvent,
+            yearlyPayment
+        }
+
+
+
+    }
+}
